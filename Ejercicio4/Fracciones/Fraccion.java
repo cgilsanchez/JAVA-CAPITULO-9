a1b0762c59e931dc12c8577b05e1dc021a96199a
cgilsanchez <cgilsanchez.02@gmail.com>
2024-01-17 20:00:19 +0100

    first commit

---
+++ Ejercicio4/Fracciones/Fraccion.java
@@ -0,0 +1,30 @@
+package Ejercicio4.Fracciones;
+
+public class Fraccion {
+
+        private int numerador;
+        private int denominador;
+
+    public Fraccion(int n, int n1){
+        this.denominador = n1;
+        this.numerador = n;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+
+        public void 
+}
